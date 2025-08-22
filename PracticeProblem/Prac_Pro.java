9a9fdd1d4b728ec41ccebde70b3e137064e8c34f
Vami112 <solankiriddhi68@gmail.com>
2025-08-22 23:16:10 +0530

    Add files via upload

---
+++ PracticeProblem/Prac_Pro.java
@@ -0,0 +1,90 @@
+// package PracticeProblem;
+
+import java.util.Scanner;
+
+public class Prac_Pro {
+   public static void main(String[] args) {
+    
+    
+    //MAIN PROBLEMS
+    // Q1
+    //System.out.println("Welcome to Bridgelabz!");
+
+    //Q2
+    // Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     int sum = a + b;
+    //     System.out.println("Sum: " + sum);
+
+    //Q3
+    // Scanner sc = new Scanner(System.in);
+    //     double c = sc.nextDouble();
+    //     double f = (c * 9/5) + 32;
+    //     System.out.println("Fahrenheit: " + f);
+
+    // Q4
+    // Scanner sc = new Scanner(System.in);
+    //     double r = sc.nextDouble();
+    //     double area = Math.PI * r * r;
+    //     System.out.println("Area: " + area);
+
+    // Q5
+    // Scanner sc = new Scanner(System.in);
+    //     double r = sc.nextDouble();
+    //     double h = sc.nextDouble();
+    //     double volume = Math.PI * r * r * h;
+    //     System.out.println("Volume: " + volume);
+
+    
+
+
+
+    // SELF PROBLEMS
+    //Q1
+    // Scanner sc = new Scanner(System.in);
+    //     double p = sc.nextDouble();
+    //     double r = sc.nextDouble();
+    //     double t = sc.nextDouble();
+    //     double si = (p * r * t) / 100;
+    //     System.out.println("Simple Interest: " + si);
+
+    //Q2
+    // Scanner sc = new Scanner(System.in);
+    //     double l = sc.nextDouble();
+    //     double w = sc.nextDouble();
+    //     double perimeter = 2 * (l + w);
+    //     System.out.println("Perimeter: " + perimeter);
+
+    //Q3
+    // Scanner sc = new Scanner(System.in);
+    //     double base = sc.nextDouble();
+    //     double exp = sc.nextDouble();
+    //     double result = Math.pow(base, exp);
+    //     System.out.println("Result: " + result);
+
+    //Q4
+    // Scanner sc = new Scanner(System.in);
+    //     double a = sc.nextDouble();
+    //     double b = sc.nextDouble();
+    //     double c = sc.nextDouble();
+    //     double avg = (a + b + c) / 3;
+    //     System.out.println("Average: " + avg);
+
+    //Q5
+    // Scanner sc = new Scanner(System.in);
+    //     double km = sc.nextDouble();
+    //     double miles = km * 0.621371;
+    //     System.out.println("Miles: " + miles);
+
+
+
+
+
+
+
+
+
+
+   } 
+}
