d57cd4b80ccb337b46c9fc3b244405bc115a224b
Vami112 <solankiriddhi68@gmail.com>
2025-08-22 23:16:48 +0530

    Add files via upload

---
+++ Programming/Assisnment.java
@@ -0,0 +1,242 @@
+// package Programming;
+
+import java.util.*;
+
+public class Assisnment {
+    public static void main(String[] args) {
+
+        
+        // Q1
+
+        // int birthYear = 2000;
+        // int currentYear = 2024;
+        // int age = currentYear - birthYear;
+        // System.out.println("Harry's age in 2024 is " + age);
+
+        // Q2
+        // int maths = 94, physics = 95, chemistry = 96;
+        // double average = (maths + physics + chemistry) / 3.0;
+        // System.out.println("Sam’s average mark in PCM is " + average);
+
+        // Q3
+        // double km = 10.8;
+        // double miles = km * 1.6;
+        // System.out.println("The distance " + km + " km in miles is " + miles);
+
+        // Q4
+        // int costPrice = 129, sellingPrice = 191;
+        // int profit = sellingPrice - costPrice;
+        // double profitPercent = (profit * 100.0) / costPrice;
+
+        // System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice +"\nThe Profit is INR " + profit + " and the Profit Percentage is " + profitPercent + "%");
+
+        //Q5
+        // int pens = 14, students = 3;
+        // int penPerStudent = pens / students;
+        // int remainingPens = pens % students;
+        // System.out.println("The Pen Per Student is " + penPerStudent + " and the remaining pen not distributed is " + remainingPens);
+
+        //Q6
+        // double fee = 125000;
+        // double discountPercent = 10;
+        // double discount = (fee * discountPercent) / 100;
+        // double finalFee = fee - discount;
+        // System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+
+        //Q7
+        // double radiusKm = 6378;
+        // double volumeKm = (4.0/3.0) * Math.PI * Math.pow(radiusKm, 3);
+        // double radiusMiles = radiusKm / 1.6;
+        // double volumeMiles = (4.0/3.0) * Math.PI * Math.pow(radiusMiles, 3);
+
+        // System.out.println("The volume of earth in cubic kilometers is " + volumeKm +" and cubic miles is " + volumeMiles);
+
+        // Q8
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter distance in kilometers: ");
+        // double km = input.nextDouble();
+        // double miles = km / 1.6;
+        // System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
+
+        // Q9
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter Course Fee: ");
+        // double fee = input.nextDouble();
+        // System.out.print("Enter Discount Percent: ");
+        // double discountPercent = input.nextDouble();
+
+        // double discount = (fee * discountPercent) / 100;
+        // double finalFee = fee - discount;
+
+        // System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+
+        
+        
+        // Q10
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter height in cm: ");
+        // double cm = input.nextDouble();
+        // double inches = cm / 2.54;
+        // int feet = (int) (inches / 12);
+        // double remainingInches = inches % 12;
+
+        // System.out.println("Your Height in cm is " + cm + " while in feet is " + feet + " and inches is " + remainingInches);
+
+
+        // Q11
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter first number: ");
+        // double num1 = input.nextDouble();
+        // System.out.print("Enter second number: ");
+        // double num2 = input.nextDouble();
+        // System.out.println("Addition = " + (num1 + num2));
+        // System.out.println("Subtraction = " + (num1 - num2));
+        // System.out.println("Multiplication = " + (num1 * num2));
+        // System.out.println("Division = " + (num1 / num2));
+
+        // Q12
+        //  Scanner input = new Scanner(System.in);
+        // System.out.print("Enter base: ");
+        // double base = input.nextDouble();
+        // System.out.print("Enter height: ");
+        // double height = input.nextDouble();
+        // double areaCm = 0.5 * base * height;
+        // double areaInches = areaCm / 6.4516;
+        // System.out.println("The area of triangle in square cm is " + areaCm + " and in square inches is " + areaInches);
+
+
+        // Q13
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter perimeter of square: ");
+        // double perimeter = input.nextDouble();
+        // double side = perimeter / 4;
+        // System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+
+
+        // Q14
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter distance in feet: ");
+        // double feet = input.nextDouble();
+        // double yards = feet / 3;
+        // double miles = yards / 1760;
+        // System.out.println("The distance in yards is " + yards + " and in miles is " + miles);
+
+        // Q15
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter unit price: ");
+        // double unitPrice = input.nextDouble();
+        // System.out.print("Enter quantity: ");
+        // int quantity = input.nextInt();
+        // double total = unitPrice * quantity;
+        // System.out.println("The total purchase price is INR " + total + " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+
+        // Q16
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter number of students: ");
+        // int n = input.nextInt();
+        // int handshakes = (n * (n - 1)) / 2;
+        // System.out.println("Maximum number of handshakes among " + n + " students is " + handshakes);
+
+
+
+
+        // LEVEL 2
+
+        // Q1
+        // Scanner sc = new Scanner(System.in);
+        // float number1 = sc.nextFloat();
+        // float number2 = sc.nextFloat();
+        // System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " + number1 + " and " + number2 + " is " + (number1 + number2) + ", " + (number1 - number2) + ", " + (number1 * number2) + ", " + (number1 / number2));
+
+        // Q2
+        // Scanner sc = new Scanner(System.in);
+        // float base = sc.nextFloat();
+        // float height = sc.nextFloat();
+        // float areaCm = 0.5f * base * height;
+        // double areaInch = areaCm / (2.54 * 2.54);
+        // System.out.println("The Area of the triangle in sq in is " + areaInch + " and sq cm is " + areaCm);
+
+        //Q3
+        // Scanner sc = new Scanner(System.in);
+        // int perimeter = sc.nextInt();
+        // int side = perimeter / 4;
+        // System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+
+        // Q4
+        // Scanner sc = new Scanner(System.in);
+        // int feet = sc.nextInt();
+        // double yards = feet / 3.0;
+        // double miles = yards / 1760.0;
+        // System.out.println("The distance in yards is " + yards + " while the distance in miles is " + miles);
+
+        // Q5
+        // Scanner sc = new Scanner(System.in);
+        // int unitPrice = sc.nextInt();
+        // int quantity = sc.nextInt();
+        // int total = unitPrice * quantity;
+        // System.out.println("The total purchase price is INR " + total + " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+
+        // Q6
+        // Scanner sc = new Scanner(System.in);
+        // int number1 = sc.nextInt();
+        // int number2 = sc.nextInt();
+        // int q = number1 / number2;
+        // int r = number1 % number2;
+        // System.out.println("The Quotient is " + q + " and Reminder is " + r + " of two number " + number1 + " and " + number2);
+
+        // Q7
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int r1 = a + b * c;
+        // int r2 = a * b + c;
+        // int r3 = c + a / b;
+        // int r4 = a % b + c;
+        // System.out.println("The results of Int Operations are " + r1 + ", " + r2 + ", " + r3 + ", and " + r4);
+
+        // Q8
+        // Scanner sc = new Scanner(System.in);
+        // double a = sc.nextDouble();
+        // double b = sc.nextDouble();
+        // double c = sc.nextDouble();
+        // double r1 = a + b * c;
+        // double r2 = a * b + c;
+        // double r3 = c + a / b;
+        // double r4 = a % b + c;
+        // System.out.println("The results of Int Operations are " + r1 + ", " + r2 + ", " + r3 + ", and " + r4);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
